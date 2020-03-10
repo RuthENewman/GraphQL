5155b21e0262560944f7fb3f510ec2406c79b05c
Ruth Newman <ruthnewman@gmail.com>
2020-03-10 00:43:03 +0000

    added relationship between posts and comments

--- src/index.js
+++ src/index.js
@@ -46,19 +46,23 @@ const posts = [{
 const comments = [{
         id: 188,
         text: "Ooh how interesting",
-        author: 2
+        author: 2,
+        post: 1
 }, {
         id: 189,
         text: "Thanks for the compliment",
-        author: 1
+        author: 1,
+        post: 1
 }, {
         id: 211,
         text: "Oooh how insightful",
-        author: 3
+        author: 3,
+        post: 2
 }, {
         id: 214,
         text: "I disagree",
-        author: 1
+        author: 1,
+        post: 3
 }];
 
 // Type definitions (schema)
@@ -86,12 +90,14 @@ const typeDefs = `
         body: String!
         published: Boolean!
         author: User!
+        comments: [Comment!]!
     }
 
     type Comment {
         id: ID!
         text: String!
         author: User!
+        post: Post!
     }
 `;
 
@@ -139,6 +145,11 @@ const resolvers = {
             return users.find((user) => {
                 return user.id === parent.author;
             });
+        },
+        comments(parent, args, context, info) {
+            return comments.filter((comment) => {
+                return comment.post === parent.id;
+            });
         }
     },
     User: {
@@ -157,6 +168,11 @@ const resolvers = {
         author(parent, args, context, info) {
             return users.find((user) => {
                 return user.id === parent.author;
+            });
+        },
+        post(parent, args, context, info) {
+            return posts.find((post) => {
+                return post.id === parent.post;
             });
         }
     }
