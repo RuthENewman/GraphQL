635ab1cb5efa693bf16ce27adcc6128fd4478a83
Ruth Newman <ruthnewman@gmail.com>
2020-03-13 00:49:35 +0000

    added mutations for updating comments and posts

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -74,6 +74,23 @@ const Mutation = {
         db.comments = db.comments.filter((comment) => comment.post !== args.id);
         return deletedPost;
     },
+    updatePost(parent, args, { db }, info) {
+        const { id, data } = args;
+        const post = db.posts.find((post) => post.id === id);
+        if (!post) {
+            throw new Error("Post not found");
+        }
+        if (typeof data.title === 'string') {
+            post.title = data.title;
+        }
+        if (typeof data.body === 'string') {
+            post.body = data.body;
+        }
+        if (typeof data.published === 'boolean') {
+            post.published = data.published;
+        }
+        return post;
+    },
     createComment(parent, args, { db }, info) {
         const userFound = db.users.find((user) => user.id === args.data.author);
         const postFound = db.posts.find((post) => post.id === args.data.post && post.published);
@@ -96,6 +113,17 @@ const Mutation = {
         }
         const deletedComment = db.comments.splice(commentIndex, 1)[0];
         return deletedComment;
+    },
+    updateComment(parent, args, { db }, info) {
+        const { id, data } = args;
+        const comment = db.comments.find((comment) => comment.id === id);
+        if (!comment) {
+            throw new Error("Comment not found");
+        }
+        if (typeof data.text === "string") {
+            comment.text = data.text;
+        }
+        return comment;
     }
 };
 
