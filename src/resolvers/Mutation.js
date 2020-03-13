628905dcfa75399f53602fcad31ce385ba12f7fa
Ruth Newman <ruthnewman@gmail.com>
2020-03-13 00:30:47 +0000

    added mutation to update a user

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -31,6 +31,27 @@ const Mutation = {
         db.comments = db.comments.filter((comment) => comment.author !== args.id);
         return deletedUser;
     },
+    updateUser(parent, args, { db }, info) {
+        const { id, data } = args;
+        const user = db.users.find((user) => user.id === id);
+        if (!user) {
+            throw new Error("User not found");
+        }
+        if (typeof data.email === 'string') {
+            const emailInUse = db.users.some((user) => user.email === data.email);
+            if (emailInUse) {
+                throw new Error("Email already in use");
+            }
+            user.email = data.email;
+        }
+        if (typeof data.name === 'string') {
+            user.name = data.name;
+        }
+        if (typeof data.age !== 'undefined') {
+            user.age = data.age;
+        }
+        return user;
+    },
     createPost(parent, args, { db }, info) {
         const author = db.users.some((user) => user.id === args.data.author);
 
